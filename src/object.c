c74f828347801c02585ab1ff4a3434a989acdb78
Rafael Costa <racosa@protonmail.com>
2016-09-23 00:59:57 +0200

    Initializing git repo.

---
+++ src/object.c
@@ -0,0 +1,87 @@
+
+/**
+ * @file object.c
+ * @author François Cayre <cayre@yiking.(null)>
+ * @date Fri Jun 15 18:02:57 2012
+ * @brief Object definitions for SFS.
+ *
+ * Object definitions for SFS.
+ */
+
+#include "object.h"
+#include "mem.h"
+
+
+object make_object( uint type ) {
+
+    object t = sfs_malloc( sizeof( *t ) );
+
+    t->type = type;
+
+    return t;
+}
+
+object make_nil( void ) {
+
+    object t = make_object( SFS_NIL );
+
+    t->this.special = t;
+
+    return t;
+}
+
+object make_boolean( void ){
+
+    object t = make_object( SFS_BOOLEAN );
+
+    t->this.boolean = t;
+
+    return t;
+}
+
+object make_integer( int integer ) {
+
+    object t = make_object( SFS_NUMBER );
+
+    t->this.number.numtype= NUM_INTEGER;
+    t->this.number.this.integer = integer;
+
+    return t;
+}
+
+object make_character( char character ){
+
+    object t = make_object( SFS_CHARACTER );
+
+    t->this.character = character;
+
+    return t;
+}
+
+object make_string ( string string ){
+
+    object t = make_object ( SFS_STRING );
+
+    strcpy( t->this.string, string );
+
+    return t;
+}
+
+object make_symbol ( string symbol ){
+
+    object t = make_object( SFS_SYMBOL );
+
+    strcpy( t->this.symbol, symbol );
+
+    return t;
+}
+
+object make_pair ( object car, object cdr ){
+
+    object t = make_object ( SFS_PAIR );
+
+    t->this.pair.car = car;
+    t->this.pair.cdr = cdr;
+
+    return t;
+}
