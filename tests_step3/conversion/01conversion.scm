c1d35b9632d24181c7c31bdfb9d44f22ade96bc2
Rafael Costa <racosa@protonmail.com>
2016-11-13 21:06:53 +0100

    Fixing file 01conversion.scm.

--- tests_step3/conversion/01conversion.scm
+++ tests_step3/conversion/01conversion.scm
@@ -1,13 +1,11 @@
 ; TEST_RETURN_CODE=PASS
 ; TEST_COMMENT="Test conversion 01 integer->char"
 
-(integer->char 1)
-(integer->char 2)
-(integer->char 3)
-(integer->char 8)
-(integer->char 9)
-(integer->char 17)
+(integer->char 32)
+(integer->char 33)
+(integer->char 38)
+(integer->char 39)
+(integer->char 37)
 (integer->char 55)
 (integer->char 111)
-(integer->char 1287)
-
+(integer->char 126)
